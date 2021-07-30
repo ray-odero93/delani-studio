8d1000023c6ecbc9681990e473dd110939519332
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-07-30 19:29:15 +0300

    Define function that will enable a hover effect upon user placing mouse over image.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   $("#imageDesign").click(function () {
-    $("#imageDesign").slideDown('').hide('');
-    $("#design").show('');
+    $("#imageDesign").slideDown("").hide("");
+    $("#design").show("");
   });
   $("#design").click(function () {
-    $("#design").slideUp('');
-    $("#design").slideDown('');
+    $("#design").slideUp("");
+    $("#design").slideDown("");
   });
 });
 
@@ -31,3 +31,82 @@ $(document).ready(function () {
   });
 });
 
+$(document).ready(function () {
+  $("#work1")
+    .mouseover(function () {
+      $("#black").show();
+    })
+    .mouseout(function () {
+      $("#black").hide();
+    });
+});
+
+$(document).ready(function () {
+  $("#work2")
+    .mouseover(function () {
+      $("#ontario").show();
+    })
+    .mouseout(function () {
+      $("#ontario").hide();
+    });
+});
+
+$(document).ready(function () {
+  $("#work3")
+    .mouseover(function () {
+      $("#gorilla").show();
+    })
+    .mouseout(function () {
+      $("#gorilla").hide();
+    });
+});
+
+$(document).ready(function () {
+  $("#work4")
+    .mouseover(function () {
+      $("#devApps").show();
+    })
+    .mouseout(function () {
+      $("#devApps").hide();
+    });
+});
+
+$(document).ready(function () {
+  $("#work5")
+    .mouseover(function () {
+      $("#userInterface").show();
+    })
+    .mouseout(function () {
+      $("#userInterface").hide();
+    });
+});
+
+$(document).ready(function () {
+  $("#work6")
+    .mouseover(function () {
+      $("#tweaks").show();
+    })
+    .mouseout(function () {
+      $("#tweaks").hide();
+    });
+});
+
+$(document).ready(function () {
+  $("#work7")
+    .mouseover(function () {
+      $("#logos").show();
+    })
+    .mouseout(function () {
+      $("#logos").hide();
+    });
+});
+
+$(document).ready(function () {
+  $("#work8")
+    .mouseover(function () {
+      $("#restaurant").show();
+    })
+    .mouseout(function () {
+      $("#restaurant").hide();
+    });
+});
